1fb84bac77d612960b658523ba076e8f1dc6766e
Andrew Hudson <andrew@ventureharbour.com>
2021-04-29 09:21:46 +0100

    ability to toggle music in office!

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,103 @@
-import * as React from "react";
+import React, { useState, useEffect, useMemo, useRef } from "react";
 import { Button, StyleSheet } from "react-native";
 
 import { Text, View } from "../components/Themed";
 
 import { styles } from "../styles/tabs";
 
+const BASE_URI = "http://192.168.86.229";
+
+const fetchOptions: any = {
+  mode: "no-cors",
+};
+
 export default function TabOneScreen() {
-  const [pressed, setPressed] = React.useState(false);
-  const [count, setCount] = React.useState(0);
+  const [pressed, setPressed] = useState(false);
+  const [count, setCount] = useState(0);
+  const [selectedRoom, setSelectedRoom] = useState("bedroom");
+  const [musicPlaying, setMusicPlaying] = useState(false);
+  const [roomVolumes, setRoomVolumes] = useState(null);
+  const [roomsMuted, setRoomsMuted] = useState(null);
+  const [volume, setVolume] = useState(0);
+
+  const volRef = useRef();
+
   const handleButtonPress = () => {
     setCount((c) => c + 1);
+  };
+
+  const toggleRoom = async () => {
+    await toggleMusic();
+  };
+
+  useEffect(() => {
+    const getAllStatuses = async () => {
+      await fetch(`${BASE_URI}/api/sonos/status-all`, fetchOptions)
+        .then((res) => res.json())
+        .then((json) => {
+          const roomsObj: any = {};
+          const soundObj: any = {};
+          const mutedObj: any = {};
+
+          json.statuses.forEach(({ room, state, volume, muted }) => {
+            roomsObj[room] = state !== "paused" && state !== "stopped";
+            soundObj[room] = volume;
+            mutedObj[room] = muted;
+          });
+
+          setRoomsMuted(mutedObj);
+          setRoomVolumes(soundObj);
+          setMusicPlaying(roomsObj);
+        })
+        .catch((e) => console.error(e));
+    };
+    getAllStatuses();
+  }, []);
+
+  const toggleMusic = async () => {
+    await fetch(
+      `${BASE_URI}/api/sonos/toggle-room/?room=${selectedRoom}`,
+      fetchOptions
+    )
+      .then((res) => res.json())
+      .then(({ state }) => {
+        setMusicPlaying((prevState) => {
+          return {
+            ...(prevState as any),
+            [selectedRoom]: state === "transitioning",
+          };
+        });
+      })
+      .catch((e) => console.error(e));
+  };
+
+  const icon = useMemo(() => {
+    const playIcon = "M11,10 L17,10 17,26 11,26 M20,10 L26,10 26,26 20,26";
+    const pauseIcon =
+      "M11,10 L18,13.74 18,22.28 11,26 M18,13.74 L26,18 26,18 18,22.28";
+
+    if (musicPlaying[selectedRoom]) {
+      return playIcon;
+    }
+    return pauseIcon;
+  }, [musicPlaying, selectedRoom]);
+
+  console.log({ icon });
+
+  const getCurrentTrack = async () => {
+    try {
+      await fetch(
+        `${BASE_URI}/api/sonos/play-room?room=${selectedRoom}`,
+        fetchOptions
+      )
+        .then((res) => res.json())
+        // .then(({ currentTrack }) => {
+        // setCurrentTrackPlaying(currentTrack);
+        // })
+        .catch((e) => console.error(e));
+    } catch (e) {
+      console.error("error in useEffect", e);
+    }
   };
 
   return (
@@ -22,9 +110,15 @@ export default function TabOneScreen() {
       />
       <Text style={styles.text}>So this is the first tab</Text>
 
-      <Button onPress={handleButtonPress} title={"Increment the number"} />
+      <Button
+        onPress={toggleRoom}
+        // className={classnames({
+        // active: musicPlaying[selectedRoom],
+        // })}
+        title={musicPlaying[selectedRoom] ? "Pause" : "Play"}
+      ></Button>
 
-      <Text style={styles.text}>{count}</Text>
+      <Text style={styles.text}>{volume}</Text>
     </View>
   );
 }
